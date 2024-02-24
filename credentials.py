f4acc5c10ffe6c2d733108f60c84a52d0f742761
Moronfoluwa Akintola <moronfoluwaakintola@gmail.com>
2024-02-25 01:54:16 +0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,18 +2,18 @@
     Twitter Credentials
 """
 
-SECRET_KEY = "asduicqdqweqweqwe9209"
+SECRET_KEY = 
 # Variables that contains the user credentials to access Twitter API
-ACCESS_TOKEN = "465633653-x7NHimVAVuMGieuVi9GIRFL8TcuCFUxSQ4NjIdHR"
-ACCESS_TOKEN_SECRET = "hSwFbdEv906tsZ67igSuKXrR8kh3yg1CDg2ElVo9ZO7dK"
-CONSUMER_KEY = "VoPZQmgqLLn9fO3TdSVgdza6q"
-CONSUMER_SECRET = "F1HEnesPeHSn514KYJNRbBFQumbRmrCf74IPsEKXJhIttj3aMT"
+ACCESS_TOKEN = 
+ACCESS_TOKEN_SECRET = 
+CONSUMER_KEY = 
+CONSUMER_SECRET = 
 
-DB_CONNECTION = "mongodb+srv://admin:VGoLDnP82lPth3KJ@analysis-vfudj.mongodb.net/test?retryWrites=true&w=majority"
+DB_CONNECTION = 
                 # ""
 DB_TWEET_COLLECTION = "sentiment_analysis"
 DB_RETRIEVED_TWEETS_RESULTS = "sentiment_analysis_results"
 DB_INSTAGRAM_COLLECTION = "instagram_sentiment_analysis"
 DB_YOUTUBE_COLLECTION = "youtube_sentiment_analysis"
 # Youtube
-YOUTUBE_API_KEY = "AIzaSyDQwKqbOEH3_YpCh6NZCpJc0CS5mBjbsF8"
+YOUTUBE_API_KEY = ""
